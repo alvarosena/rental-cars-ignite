d5a17583614fec7c9d5c15371a85dd6dce835337
Alvaro Sena <alvarosenacs.c@gmail.com>
2021-08-17 19:51:55 -0300

    refactoring of categoryAlreadyExists

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,24 @@
-import { Router } from 'express';
-import { Category } from '../model/Category';
+import { response, Router } from 'express';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
     
-    const category = new Category();
+    const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-    Object.assign(category, {
-        name,
-        description,
-        createdAt: new Date()
-    })
-
-    categories.push(category);
+    createCategoryService.execute({name, description});
 
     return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+    const all = categoriesRepository.list();
+
+    return response.json(all);
 });
 
 export { categoriesRoutes }
